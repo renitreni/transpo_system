f0422a270e35b48bfeaeefeb4547f885d488453f
renier.trenuela@freighttrackertms.com <renier.trenuela@freighttrackertms.com>
2025-11-20 23:24:12 +0800

    Adding glow radius

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,26 +11,26 @@ export default {
       keyframes: {
         'glow-red': {
           '0%, 100%': { 
-            boxShadow: '0 0 5px rgba(239, 68, 68, 0.5)'
+            boxShadow: '0 0 5px 2px rgba(239, 68, 68, 0.5)'
           },
           '50%': { 
-            boxShadow: '0 0 20px rgba(239, 68, 68, 0.8),'
+            boxShadow: '0 0 20px 4px rgba(231, 9, 9, 0.8)'
           },
         },
         'glow-yellow': {
           '0%, 100%': { 
-            boxShadow: '0 0 5px rgba(234, 179, 8, 0.5)'
+            boxShadow: '0 0 5px 2px rgba(234, 179, 8, 0.5)'
           },
           '50%': { 
-            boxShadow: '0 0 30px rgba(234, 179, 8, 0.6)'
+            boxShadow: '0 0 30px 4px rgba(234, 179, 8, 0.6)'
           },
         },
         'glow-green': {
           '0%, 100%': { 
-            boxShadow: '0 0 5px rgba(34, 197, 94, 0.5)'
+            boxShadow: '0 0 5px 2px rgba(34, 197, 94, 0.5)'
           },
           '50%': { 
-            boxShadow: '0 0 30px rgba(34, 197, 94, 0.6)'
+            boxShadow: '0 0 30px 4px rgba(34, 197, 94, 0.6)'
           },
         },
       },
